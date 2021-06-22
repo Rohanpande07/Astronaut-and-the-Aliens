d28bc19e0f167b0a881cff221ee8afd91fdda58b
Rohanpande07 <78965170+Rohanpande07@users.noreply.github.com>
2021-06-22 15:36:33 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Astronaut and the Aliens
+https://github.com/Rohanpande07/Astronaut-and-the-Aliens.git
---
+++ sketch.js
@@ -0,0 +1,47 @@
+ var astronaut, astronautImg;
+ var bg;
+ var laser, laserIMG;
+
+ function preload(){
+     astronautImg = loadImage("images/astronaut3.png");
+     bg = loadImage("images/bg/space4.jpg");
+     laserIMG = loadImage("images/lasers/laser_red.png");
+ }
+
+
+ function setup(){
+     createCanvas(1000,500);
+
+     astronaut = createSprite(100,250,50,50);
+     astronaut.addImage(astronautImg);
+     astronaut.scale = 0.5;
+   
+     laser = createSprite(215,226,50,50);
+     laser.addImage(laserIMG);
+     laser.scale = 0.05;
+ }
+ function draw(){
+     background(bg);
+
+     if(keyDown("left")){
+         astronaut.x -= 5;
+         laser.x -= 5;
+     }
+
+     if(keyDown("right")){
+        astronaut.x += 5;
+        laser.x += 5;
+    }
+
+     if(keyDown("up")){
+        astronaut.y -= 5;
+        laser.y -= 5;
+    }
+   
+     if(keyDown("down")){
+        astronaut.y += 5;
+        laser.y += 5;
+    }
+
+     drawSprites();
+ }
